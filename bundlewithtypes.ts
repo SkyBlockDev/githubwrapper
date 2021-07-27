9751595a6a524d73a8a2f06ed63b8f538b46d438
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-07-27 17:58:11 +0200

    Remove broken types

--- bundlewithtypes.ts
+++ bundlewithtypes.ts
@@ -1,13 +1,14 @@
-const data = await Deno.emit("mod.ts", {
-  compilerOptions: {
-    declaration: true,
-    emitDeclarationOnly: true,
-    declarationDir: "./index.d.ts",
-  },
-});
-Deno.writeTextFileSync(
-  "dist/index.d.ts",
-  Object.entries(data.files)
-    .map((x) => (x[1].includes("export * from") || x[1].includes("import type {") ? "" : x[1]))
-    .join("\n")
-);
+// CURRENTLY CREATING TYPES DOESNT INCLUDE FUNCTIONS
+// const data = await Deno.emit("mod.ts", {
+//   compilerOptions: {
+//     declaration: true,
+//     emitDeclarationOnly: true,
+//     declarationDir: "./index.d.ts",
+//   },
+// });
+// Deno.writeTextFileSync(
+//   "dist/index.d.ts",
+//   Object.entries(data.files)
+//     .map((x) => (x[1].includes("export * from") || x[1].includes("import type {") ? "" : x[1]))
+//     .join("\n")
+// );
