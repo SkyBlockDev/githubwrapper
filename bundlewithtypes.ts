a0f1d48702bc845e5ca2f32b3bc47a649b588d93
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-07-28 15:28:58 +0200

    Add types and mjs support

--- bundlewithtypes.ts
+++ bundlewithtypes.ts
@@ -1,14 +1,14 @@
-// CURRENTLY CREATING TYPES DOESNT INCLUDE FUNCTIONS
-// const data = await Deno.emit("mod.ts", {
-//   compilerOptions: {
-//     declaration: true,
-//     emitDeclarationOnly: true,
-//     declarationDir: "./index.d.ts",
-//   },
-// });
-// Deno.writeTextFileSync(
-//   "dist/index.d.ts",
-//   Object.entries(data.files)
-//     .map((x) => (x[1].includes("export * from") || x[1].includes("import type {") ? "" : x[1]))
-//     .join("\n")
-// );
+const data = await Deno.emit("mod.ts", {
+  compilerOptions: {
+    declaration: true,
+    emitDeclarationOnly: true,
+    declarationDir: "./",
+  },
+});
+
+Deno.writeTextFileSync(
+  "dist/index.d.ts",
+  Object.entries(data.files)
+    .map((x) => x[1])
+    .join("\n")
+);
